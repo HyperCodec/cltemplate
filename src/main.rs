652bb7e1a6b4d631bdaa304b89790eea963ae83f
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2023-11-30 13:24:32 +0000

    add indicatif multibar spinner

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use error::Result;
 use async_recursion::async_recursion;
 use clap::Parser;
 use dialoguer::{theme::ColorfulTheme, Input};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
@@ -13,7 +14,7 @@ use std::{
     path::PathBuf,
     str::FromStr,
     sync::{Arc, Mutex},
-    time::Instant,
+    time::{Instant, Duration},
 };
 use tracing::{debug, error, info};
 use tracing_subscriber::EnvFilter;
@@ -79,8 +80,9 @@ async fn main() -> Result<()> {
     drop(items);
 
     info!("Beginning template copying process");
+    let progress = Arc::new(MultiProgress::new());
     let start = Instant::now();
-    template_async(template.clone(), output, template).await?;
+    template_async(template.clone(), output, template, progress.clone()).await?;
 
     info!(
         "Template copied successfully (elapsed: {:#?})",
@@ -95,11 +97,41 @@ async fn template_async(
     path: PathBuf,
     current_output: PathBuf,
     template_path: PathBuf,
+    multiprogress: Arc<MultiProgress>,
 ) -> Result<()> {
     debug!("Worker started");
 
-    let subdirs = fs::read_dir(path)?;
+    let subdirs = fs::read_dir(&path)?;
     let mut handles = Vec::new();
+
+    // init progress spinner
+    let pb = multiprogress.add(ProgressBar::new_spinner());
+    pb.set_message(path.strip_prefix(&template_path).unwrap().display().to_string());
+    pb.enable_steady_tick(Duration::from_millis(128));
+    pb.set_style(
+        ProgressStyle::with_template("[{elapsed_precise}] {spinner:} {msg}")
+        .unwrap()
+        .tick_strings(&[
+			"⠋",
+			"⠙",
+			"⠚",
+			"⠒",
+			"⠂",
+			"⠂",
+			"⠒",
+			"⠲",
+			"⠴",
+			"⠦",
+			"⠖",
+			"⠒",
+			"⠐",
+			"⠐",
+			"⠒",
+			"⠓",
+			"⠋",
+            "✓", // TODO separate finished color
+		])
+    );
 
     // try create current dir
     fs::create_dir_all(&current_output).ok();
@@ -114,10 +146,12 @@ async fn template_async(
                 continue;
             }
 
+            /*
             info!(
                 "Copying {}",
                 path.strip_prefix(&template_path).unwrap().display()
             );
+            */
 
             // file stuff
             let content = fs::read(&path)?;
@@ -144,21 +178,26 @@ async fn template_async(
         }
 
         // directory
+        /*
         info!(
             "Copying {}",
             path.strip_prefix(&template_path).unwrap().display()
         );
+        */
 
         let new_output = current_output.join(dir.file_name());
         let template_path2 = template_path.clone();
+        let multiprogress2 = multiprogress.clone();
         handles.push(tokio::spawn(async move {
-            template_async(path, new_output, template_path2).await
+            template_async(path, new_output, template_path2, multiprogress2).await
         }));
     }
 
     for h in handles {
         h.await??;
     }
+
+    pb.finish();
 
     debug!("Worker finished");
 
